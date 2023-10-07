d2a55d86174e47f1527b876f518c10e103436a4a
David <davidbish2002@hotmail.com>
2023-10-07 00:28:33 -0230

    Status component and added the optional coins in the background for the Layout component. (The status component is the bar with the timer that appears in multiple pages.)

--- src/components/layout/index.tsx
+++ src/components/layout/index.tsx
@@ -1,16 +1,39 @@
 import React from "react";
 import Notification from "@components/notification";
 import s from "./styles.module.css";
+import coins1 from "@static/coins_variant1.png";
+import coins2 from "@static/coins_variant2.png";
+import coins3 from "@static/coins_variant3.png";
 
 interface Props {
   children?: any;
   large?: Boolean;
+  addCoins?: "coins1" | "coins2" | "coins3";
 }
 
-const Layout: React.FC<Props> = ({ children, large }) => {
+const Layout: React.FC<Props> = ({ children, large, addCoins }) => {
   return (
     <>
       <div className={large ? s.container + " " + s.large : s.container}>
+        {addCoins && (
+          <img
+            src={
+              addCoins === "coins1"
+                ? coins1
+                : addCoins === "coins2"
+                ? coins2
+                : coins3
+            }
+            alt="Doblons"
+            className={
+              addCoins === "coins1"
+                ? s.coins1
+                : addCoins === "coins2"
+                ? s.coins2
+                : s.coins3
+            }
+          />
+        )}
         {children}
       </div>
       <Notification />
