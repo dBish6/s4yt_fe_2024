e24b5f0c6a17439253d63dd20990a7f486c510de
David <davidbish2002@hotmail.com>
2023-10-07 00:29:41 -0230

    Sponsors route added.

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,29 +1,34 @@
-import Gate from '@components/gate';
-import Home from '@views/home';
-import Login from '@views/login';
-import Register from '@views/register';
-import Profile from '@views/profile';
-import PasswordReset from '@views/password/reset';
+import Gate from "@components/gate";
+import Home from "@views/home";
+import Login from "@views/login";
+import Register from "@views/register";
+import Profile from "@views/profile";
+import Sponsors from "@root/views/sponsors";
+import PasswordReset from "@views/password/reset";
 
 export default [
-	{
-    	path: "/login",
-    	element: <Gate view={<Login />} restricted={0} />
-  	},
-  	{
-    	path: "/register",
-    	element: <Gate view={<Register />} restricted={0} />
-  	},
-  	{
-    	path: "/password-reset",
-    	element: <Gate view={<PasswordReset />} restricted={0} />
-  	},
-	{
-    	path: "/",
-    	element: <Gate view={<Home />} restricted={1} />
-  	},
-  	{
-    	path: "/profile",
-    	element:<Gate view={<Profile />} restricted={1} />
-  	}
+  {
+    path: "/login",
+    element: <Gate view={<Login />} restricted={0} />,
+  },
+  {
+    path: "/register",
+    element: <Gate view={<Register />} restricted={0} />,
+  },
+  {
+    path: "/password-reset",
+    element: <Gate view={<PasswordReset />} restricted={0} />,
+  },
+  {
+    path: "/",
+    element: <Gate view={<Home />} restricted={1} />,
+  },
+  {
+    path: "/profile",
+    element: <Gate view={<Profile />} restricted={1} />,
+  },
+  {
+    path: "/sponsors",
+    element: <Gate view={<Sponsors />} restricted={1} />,
+  },
 ];
