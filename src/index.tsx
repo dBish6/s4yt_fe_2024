086acfbad0fdbfb15e288710f80e6351cb9cfe54
David <davidbish2002@hotmail.com>
2023-10-19 15:33:56 -0230

    App.css name change and fonts

--- src/index.tsx
+++ src/index.tsx
@@ -1,20 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import {PersistGate} from 'redux-persist/integration/react';
-import {Provider} from 'react-redux';
-import {Store,Persistor} from '@root/store';
-import Routes from '@root/routes';
-import './styles.css';
+import { PersistGate } from "redux-persist/integration/react";
+import { Provider } from "react-redux";
+import { Store, Persistor } from "@root/store";
+import Routes from "@root/routes";
+import "./index.css";
 
 const router = createBrowserRouter(Routes);
 
-ReactDOM.createRoot(
-    document.getElementById('root') as HTMLElement
-).render(
-    <Provider store={Store}>
-        <PersistGate loading={null} persistor={Persistor}>
-            <RouterProvider router={router} />
-        </PersistGate>
-    </Provider>
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+  <Provider store={Store}>
+    <PersistGate loading={null} persistor={Persistor}>
+      <RouterProvider router={router} />
+    </PersistGate>
+  </Provider>
 );
