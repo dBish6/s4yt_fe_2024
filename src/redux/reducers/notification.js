fe6a68e78cb5a71020315eb922f931b4acdcf037
David <davidbish2002@hotmail.com>
2023-11-01 19:14:38 -0230

    New Redux directory.

---
+++ src/redux/reducers/notification.js
@@ -0,0 +1,30 @@
+import { SET_NOTIFICATION_DATA } from "@selectors";
+
+/*
+
+	Initial Local state
+
+*/
+const initialState = {
+  display: false,
+  error: null,
+  content: null,
+  timed: true,
+};
+
+const Options = (state = initialState, action) => {
+  switch (action.type) {
+    case SET_NOTIFICATION_DATA:
+      return {
+        ...state,
+        display: action.data.display,
+        error: action.data.error,
+        content: action.data.content,
+        timed: action.data.timed,
+      };
+    default:
+      return state;
+  }
+};
+
+export default Options;
