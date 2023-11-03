3e15d3f217469639e491b5e98c8de96595fa08c4
David <davidbish2002@hotmail.com>
2023-11-02 22:27:48 -0230

    Added error pages.

---
+++ src/views/errors/Error404.tsx
@@ -0,0 +1,47 @@
+import { Link } from "react-router-dom";
+import Layout from "@components/layout";
+import Header from "@components/header";
+import Content from "@components/content";
+import s from "./styles.module.css";
+import errorLogo from "@static/error-logo.png";
+
+const Error404: React.FC = () => {
+  //   const navigate = useNavigate();
+
+  return (
+    <Layout
+    // addCoins="coins2"
+    // addFeather="right1"
+    >
+      <Header />
+      <Content
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          gap: "1.5rem 0.5rem",
+          flexWrap: "wrap-reverse",
+          paddingTop: "5rem",
+          paddingBottom: "1rem",
+        }}
+      >
+        <img src={errorLogo} alt="Error Pirate" className={s.errorLogo} />
+        <div className={s.text}>
+          <h1>
+            Error
+            <br /> 404
+          </h1>
+          <h4>The requested page was not found</h4>
+          <Link aria-label="Back to Main Map" to="/" className="fade move" />
+        </div>
+        <Link
+          aria-label="Back to Main Map"
+          to="/"
+          className={s.responsiveBack}
+        />
+      </Content>
+    </Layout>
+  );
+};
+
+export default Error404;
