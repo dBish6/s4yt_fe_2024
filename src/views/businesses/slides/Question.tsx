7bac92322f1bf9b53b055ea66fdaaf060e655572
Koki <kokicodes@gmail.com>
2023-12-19 14:41:16 -0500

    added a form and embeddede youtube iframe for business details slides

--- src/views/businesses/slides/Question.tsx
+++ src/views/businesses/slides/Question.tsx
@@ -1,11 +1,31 @@
 import { connect } from "react-redux";
-import React, { useState } from "react";
+import React, { useState, ChangeEvent, FormEvent } from "react";
 import s from "./styles.module.css";
 
 const Questions: React.FC = () => {
+  const [answer, setAnswer] = useState<string>("");
+
+  const handleInputChange = (e: ChangeEvent<HTMLTextAreaElement>) => {
+    setAnswer(e.target.value);
+  };
+
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+  };
   return (
     <div className={s.optionsView}>
-      <h2>QESTIONS</h2>
+      <form onSubmit={handleSubmit}>
+        <label>
+          Question: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eveniet, sint!"
+
+        </label>
+        <textarea
+          value={answer}
+          onChange={handleInputChange}
+          placeholder="Type your answer here"
+        />
+        <button type="submit">Submit</button>
+      </form>
     </div>
   );
 };
