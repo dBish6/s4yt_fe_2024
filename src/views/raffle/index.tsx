d99c6254726c3b867261a897b18c36550b5a6d47
David <davidbish2002@hotmail.com>
2024-01-03 20:48:49 -0330

    The raffle changes that I left.

--- src/views/raffle/index.tsx
+++ src/views/raffle/index.tsx
@@ -1,8 +1,11 @@
+import { useState } from "react";
 import { connect } from "react-redux";
+
 import Layout from "@components/layout";
 import Header from "@components/header";
 import Content from "@components/content";
 import Status from "@components/status";
+
 import s from "./styles.module.css";
 
 interface Props {}
@@ -40,9 +43,24 @@ const products = [
     img: require("@static/error-logo.png"),
     name: "T-Shirt",
   },
+  // {
+  //   img: require("@static/error-logo.png"),
+  //   name: "T-Shirt",
+  // },
+  // {
+  //   img: require("@static/error-logo.png"),
+  //   name: "T-Shirt",
+  // },
+  // {
+  //   img: require("@static/error-logo.png"),
+  //   name: "T-Shirt",
+  // },
 ];
 
 const Raffle: React.FC<Props> = ({}) => {
+  const [slideIndex, setSlideIndex] = useState(0),
+    [isOpened, setIsOpened] = useState(0);
+
   return (
     <Layout
     // addFeather="right2"
@@ -64,14 +82,26 @@ const Raffle: React.FC<Props> = ({}) => {
             </h2>
             {/* TODO: The total is their current doblons. */}
             <h4>
-              Your Total: <span>1</span>
+              Your Total Dubl-u-nes: <span>1</span>
             </h4>
+            <div className={s.legend}>
+              <h4>Legend</h4>
+              <div>
+                <img src="" alt="" />
+                <p></p>
+              </div>
+              <div>
+                <img src="" alt="" />
+                <p></p>
+              </div>
+            </div>
           </div>
           <div className={s.products}>
-            {products.map((item, i) => (
+            {products.slice(0, 8).map((item, i) => (
               <div aria-label={item.name} key={i}>
                 <div className={s.imgBox}>
                   <img src={item.img} alt={item.name} />
+                  <button />
                 </div>
                 <h4 className={s.name}>{item.name}</h4>
                 <div className={s.controls}>
@@ -82,6 +112,7 @@ const Raffle: React.FC<Props> = ({}) => {
               </div>
             ))}
           </div>
+          <button />
         </div>
       </Content>
       <Status />
