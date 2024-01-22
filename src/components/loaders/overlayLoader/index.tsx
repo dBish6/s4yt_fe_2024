5b0b95969d0f1445a3bd2265c814b1a06e35c3e0
David <davidbish2002@hotmail.com>
2024-01-22 19:00:34 -0330

    Login restrictedAccess for non-backup users when the game haven't started yet fix, and fixed how :focus didn't work correctly with our select img + small touch ups.

--- src/components/loaders/overlayLoader/index.tsx
+++ src/components/loaders/overlayLoader/index.tsx
@@ -7,7 +7,7 @@ interface Props {
 const OverlayLoader: React.FC<Props> = ({ text }) => {
   return (
     <div role="presentation" className={`${s.backdrop} backdrop`}>
-      <div role="status" className={s.loaderContainer}>
+      <div role="status" className={`overlayLoader ${s.loaderContainer}`}>
         <div className={s.text}>{text}...</div>
         <span aria-label="Loading Indicator" className={s.loader} />
       </div>
