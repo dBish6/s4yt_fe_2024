e3929583af0f7643d4ecf028d6380af41ac49ad6
Koki <kokicodes@gmail.com>
2024-01-23 18:31:41 -0500

    static file change and db config fix

--- src/firebase.js
+++ src/firebase.js
@@ -1,7 +1,5 @@
 import { initializeApp } from "firebase/app";
 import { getDatabase } from "firebase/database";
-import { getAuth } from "firebase/auth";
-import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_FIREBASE_API,
@@ -14,6 +12,4 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 const db = getDatabase(app);
-const auth = getAuth(app);
-const storage = getStorage(app);
-export { db, auth, storage };
+export { db };
