10fcf47e2aee1b910e3cc2767ef4a48259e214d1
David Bishop <davidbish2002@hotmail.com>
2024-02-02 16:05:55 -0330

    New showError function added to errorHandler.js

--- src/services/errorHandler.js
+++ src/services/errorHandler.js
@@ -1,6 +1,25 @@
+import { addNotification } from "@actions/notifications";
 import history from "@utils/History";
 
 export default (func, error) => {
   console.error(`${func} Request ERROR:\n`, error);
   history.push("/error-500");
+};
+
+export const showError = (res, dispatch, customMessage) => {
+  dispatch(
+    addNotification({
+      error: true,
+      content:
+        res.errors && Object.keys(res.errors).length
+          ? res.errors[Object.keys(res.errors)[0]][0]
+          : res.message
+          ? res.message
+          : customMessage
+          ? customMessage
+          : "Unexpected Error",
+      close: false,
+      duration: 0,
+    })
+  );
 };
